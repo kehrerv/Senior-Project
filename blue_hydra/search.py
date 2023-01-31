33dd7f48fd09689c0ca19cb204671946c4c71f9e
Valerie <valpack22@gmail.com>
2023-01-31 12:30:49 -0500

    updated search.py

--- blue_hydra/search.py
+++ blue_hydra/search.py
@@ -1,27 +1,25 @@
-#/usr/bin/env python
+#!usr/bin/env python
 import os
 
 
-int far
-int measuredPwr #change this once you find out what the measured pwr actually is
-int n = 0
 
 #opens file to read "r"
 with open(r"blue_hydra_rssi.log", "r") as file:
-    #x = input("Enter target Mac address: ")
+    x = input("Enter target Mac address: ")
+
     #reads all the content from the file
-    
-    content = file.readlines()[2] #reads from a particular line from []
+    content = file.readlines()[10] #reads from a particular line from []
     characterNum = len(content)
     print("Selected line content: ", content)
     print("number of characters: ", characterNum)
-    
-    
+        
     one, two, mac, rssi = content.split()
     print("BT MAC: ", mac)
     print("RSSI: ", rssi)
 
-    '''#searches if the input is present in the text
+
+
+    #searches if the input is present in the text
     if x in content:
         print("string exist")
 
@@ -29,13 +27,8 @@ with open(r"blue_hydra_rssi.log", "r") as file:
         print(x)
     else:
         print("string does not exist")
-    '''
     
-    #calculating distance by RSSI
-
-
-    dist = 10^((measuredPwr - rssi)/(10*n))
-    print("Distance from target: ", dist)
+    
 
     
 
