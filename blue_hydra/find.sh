ba673814f3afd413e6de6b4abe742bb9f6092105
Valerie <valpack22@gmail.com>
2023-04-16 17:21:04 -0400

    updates

--- blue_hydra/find.sh
+++ blue_hydra/find.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./bin/blue_hydra > /dev/null 2>&1 #catches output and any error messages into the void aka
+#./bin/blue_hydra > /dev/null 2>&1 #catches output and any error messages into the void aka
 
-#gnome-terminal --wait --command="./bin/blue_hydra"
+gnome-terminal --command="./bin/blue_hydra"
 sleep 10
 
 ./rerun.sh
